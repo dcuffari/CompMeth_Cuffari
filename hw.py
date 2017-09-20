e8a61a457ec85da7ba67f99f71f0c304a8985cc8
dcuffari <davidcuffari@yahoo.com>
2017-09-20 13:51:10 -0400

    Finished HW 2 Problem 2

--- hw.py
+++ hw.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
 import sys,os
 import platform as pform
 import numpy as np
@@ -8,7 +9,6 @@ import matplotlib
 rows, col = os.popen('stty size', 'r').read().split()
 rows = int(rows)/2
 col = int(col)/2
-
 
 def clearscreen():
     if pform.system() == 'Linux':
@@ -20,15 +20,23 @@ def clearscreen():
 
 
 def homework1_1():
-    test = float(input('Please enter a tower height: '))
-    print("It will take {first:2.2f} seconds for the ball to hit the ground from a height of {second:2.2f} meter(s).\n".format( first=(np.sqrt(2*test/(9.8))),second=(test) ) )
+    while True:
+        try:
+            test = float(input('Please enter a tower height: '))
+            if test > 0:
+                print("It will take {first:2.2f} second(s) for the ball to hit the ground from a height of {second:2.2f} meter(s).\n".format( first=(np.sqrt(2*test/(9.8))),second=(test) ) )
+                break
+            else:
+                print("That is not a valid height")
+        except(TypeError, NameError, SyntaxError):
+            print("That is not a valid height")
 
 
 def homework1_2():
 
     def T(test):
         if test == -1:
-            test = float(input('Please enter an orbital period in seconds: '))
+            test = float(input('\nPlease enter an orbital period in seconds: '))
         G = 6.67e-11 #Newton's Gravitational Constant in m**3 kg**-1 s**-2
         M = 5.97e24  #Mass of earth in kg
         R = 6371000  #Earths radius in m
@@ -38,7 +46,7 @@ def homework1_2():
     def choose():
         while True:
             try:
-                choice = input('Would you like to try again? (1 for yes or 2 for no): ')
+                choice = int(input('Would you like to try again? (1 for yes or 2 for no): '))
                 if choice == 1 or choice == 2:
                     return choice
                 else:
@@ -49,7 +57,7 @@ def homework1_2():
 
 
     def factoid():
-        print("Before you leave, did you know the altitude for a 90 minute orbit is {first:2.2f} meters \nand {second:2.2f} meters for a 45 minute orbit.\n".format( first=(T(90*60)),second=(T(45*60)) ))
+        print("\nBefore you leave, did you know the altitude for a 90 minute orbit is {first:2.2f} meters \nand {second:2.2f} meters for a 45 minute orbit.\n".format( first=(T(90*60)),second=(T(45*60)) ))
         print("Also, the altitude for a geosynchronous (23.93 hours) satellite is {first:2.2f} meters,\nwhile a 24 hour satellite is {second:2.2f} meters.".format( first=(T(23.93*60*60)),second=(T(24*60*60)) ))
         print("That's a difference of {0:2.2f} meters.\n".format( T(24*60*60)-T(23.93*60*60) ) )
 
@@ -58,7 +66,7 @@ def homework1_2():
         try:
             Tp = T(-1)
             if Tp < 2000000:
-                os.system("echo -e '\007'")
+                os.system("echo -e '\007'")                
                 print("Your orbital period will result in an unstable orbit and crash into earth.\n")
                 choice = choose()
                 if choice == 1:
@@ -94,7 +102,7 @@ def homework2_1():
                   "\n2) Galilean Spiral"
                   "\n3) Fey's Function"
                   "\n4) Exit\n")
-            test = input("Please choose a plot: ")
+            test = int(input("Please choose a plot: "))
             if test == 1:
                 x = np.linspace(0, 2*np.pi, 100)
                 plt.plot(2*np.cos(x) + np.cos(2*x), 2*np.sin(x) - np.sin(2*x),color='black')
@@ -110,7 +118,6 @@ def homework2_1():
                 plt.title("Galilean Spiral")
                 plt.show()
             elif test == 3:
-                print("Fey chosen")
                 y = np.linspace(0,24,1000)
                 plt.plot( (np.exp(np.cos(y)) - 2*np.cos(4*y) + (np.sin(y/12))**5)*np.cos(y), (np.exp(np.cos(y)) - 2*np.cos(4*y) + (np.sin(y/12))**5)*np.sin(y) ,color='blue')
                 plt.xlabel("exp(cos(theta)) - 2cos(4theta) + (sin(theta/12))^5")
@@ -125,6 +132,42 @@ def homework2_1():
             print("\nThat is not a valid option\n")
 
 
+def homework2_2():
+
+    def feig(r,x):
+        return r*x*(1-x)
+
+
+    def feigrun(r,n,x):
+        temp = 0
+        for i in range(n):
+            temp = feig(r,x)
+            x = temp
+        return temp
+
+
+    def feigtree(r,ni,nj,x):
+        for i in range(ni):
+            x2[i] = feigrun( (i*r)/ni, nj,x)
+            x = np.random.rand()
+        return x2
+
+    
+    x = 0.5
+    ni = 5000
+    nj = 1000
+    r = 4.0
+    x2 = np.zeros(ni)
+    clearscreen()
+    print("Plotting Feigenbaum Plot")
+    plt.plot(np.linspace(0.0,r,ni),feigtree(r,ni,nj,x),'k*',ms=1)
+    plt.title("Feigenbaum Plot")
+    plt.xlabel("r")
+    plt.ylabel("x")
+    plt.axis([1,4,0,1])
+    plt.show()
+
+
 def homework3_1():
     print("hw 3.1 Not completed yet\n")
 
@@ -137,43 +180,56 @@ def homework4_1():
     print("hw 4.1 Not completed yet\n")
 
 
-while True:  # While loop structure used for error handling.
+def homework4_2():
+    print("hw 4.2 Not completed yet\n")
+
+reset = True
+while reset:  # While loop structure used for error handling.
     try:
         hwnum = 0
         print("\n    1)  HW 1 problem 1"
-              "\n    2)  HW 1 problem 2"
+              "\n    2)  HW 1 problem 2\n"
               "\n    3)  HW 2 problem 1"
-              "\n    4)  HW 3 problem 1"
-              "\n    5)  HW 3 problem 2"
-              "\n    6)  HW 4 problem 1"
-              "\n    7)  Exit\n")
-        hwnum = input("Please choose which HW assignment: ")
+              "\n    4)  HW 2 problem 2\n"
+              "\n    5)  HW 3 problem 1"
+              "\n    6)  HW 3 problem 2\n"
+              "\n    7)  HW 4 problem 1"
+              "\n    8)  HW 4 problem 2\n"
+              "\n\n    9)  Exit\n")
+        hwnum = int(input("Please choose which HW assignment: "))
         if hwnum == 1:
             clearscreen()
             homework1_1()
-        if hwnum == 2:
+        elif hwnum == 2:
             clearscreen()
             homework1_2()
-        if hwnum == 3:
+        elif hwnum == 3:
             clearscreen()
             homework2_1()
-        if hwnum == 4:
+        elif hwnum == 4:
+            clearscreen()
+            homework2_2()
+        elif hwnum == 5:
             clearscreen()
             homework3_1()
-        if hwnum == 5:
+        elif hwnum == 6:
             clearscreen()
             homework3_2()
-        if hwnum == 6:
+        elif hwnum == 7:
             clearscreen()
             homework4_1()
-        if hwnum == 7:
+        elif hwnum == 8:
             clearscreen()
+            homework4_2()
+        elif hwnum == 9:
+            clearscreen()
+            reset = False
             break
-        if not isinstance(hwnum,int):
+        elif not isinstance(hwnum,int):
             clearscreen()
             print("Please choose a value from the list\n")
         else:
-#            clearscreen()
+            clearscreen()
             print("Please choose a value from the list\n")
     except(TypeError, NameError, SyntaxError):
         clearscreen()
